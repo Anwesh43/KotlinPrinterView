bfe9333d563633e2e7214b37138e4737a31dbc17
Anwesh43 <anweshthecool0@gmail.com>
2018-05-14 08:19:14 +0530

    adding View in activity

--- app/src/main/java/com/example/anweshmishra/kotlinprinterview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinprinterview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.anweshmishra.kotlinprinterview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.printerview.PrinterView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PrinterView.create(this)
     }
 }
