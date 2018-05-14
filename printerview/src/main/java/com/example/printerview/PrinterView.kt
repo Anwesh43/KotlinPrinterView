50eff36292ae6cc4a8b37c79f735245a2a41917d
Anwesh43 <anweshthecool0@gmail.com>
2018-05-14 07:47:59 +0530

    adding printerview module

---
+++ printerview/src/main/java/com/example/printerview/PrinterView.kt
@@ -0,0 +1,10 @@
+package com.example.printerview
+
+/**
+ * Created by anweshmishra on 14/05/18.
+ */
+
+import android.content.Context
+import android.graphics.*
+import android.view.View
+import android.view.MotionEvent
