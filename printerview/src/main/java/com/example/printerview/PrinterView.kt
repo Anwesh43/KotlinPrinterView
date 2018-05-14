42c9226707b579b20c775aee4d8e1c35195c9ad5
Anwesh43 <anweshthecool0@gmail.com>
2018-05-14 07:51:50 +0530

    adding printerview module

--- printerview/src/main/java/com/example/printerview/PrinterView.kt
+++ printerview/src/main/java/com/example/printerview/PrinterView.kt
@@ -8,3 +8,22 @@ import android.content.Context
 import android.graphics.*
 import android.view.View
 import android.view.MotionEvent
+
+class PrinterView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
